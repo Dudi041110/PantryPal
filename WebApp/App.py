73b1453987911abb5b70ab50fb61a3bf69e2ec04
Dudi041110 <adam.iqran@gmail.com>
2026-05-06 22:56:38 +0400

    Some basic additions to the WebApp

--- WebApp/App.py
+++ WebApp/App.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+Customizable_AI_Skins = ["Cat", "Top", "Chef", "Robo", "Default"]
+Equipped_AI = Customizable_AI_Skins[0]
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/get_result")
+def send_result():
+    return jsonify({"result": Equipped_AI})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- WebApp/static/script.js
+++ WebApp/static/script.js
@@ -1,1 +1,28 @@
-//Don't delete this comment
+const AI_img = document.getElementById("AI_Assistant");
+const AIBG = document.getElementById("AIBG");
+
+const AI_Images = {
+    Cat: "/static/ai_assets/CatAI.png",
+    Top: "/static/ai_assets/TopHatAI.png",
+    Chef: "/static/ai_assets/ChefHatAI.png",
+    Robo: "/static/ai_assets/RoboChefAI.png",
+    Default: "/static/ai_assets/AbstractAI.png"
+};
+
+function getResult() {
+    fetch("/get_result")
+        .then(response => response.json())
+        .then(data => {
+            AI_img.src = AI_Images[data.result] || AI_Images.Default;
+            if (AI_img.src === "/static/ai_assets/CatAI.png") {
+                AIBG.classList.remove("hidden");
+                AIBG.classList.add("visible");
+            }
+            else {
+                AIBG.classList.remove("visible");
+                AIBG.classList.add("hidden");
+            }
+        });
+}
+
+getResult();
